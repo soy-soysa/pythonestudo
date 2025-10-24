1e44a1d0659c0e2aabd6ea85a4206eeb6681caac
RODRIGO DE SOUSA COSTA <rodrigo.scosta22@senacsp.edu.br>
2025-10-24 17:09:13 -0300

    Continuação da janela de login

--- Janelas-Python/logincadastro.py
+++ Janelas-Python/logincadastro.py
@@ -2,7 +2,7 @@ import sys
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QIcon, QFont
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QInputDialog, QMessageBox, QVBoxLayout, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QInputDialog, QMessageBox, QVBoxLayout, QHBoxLayout, QCheckBox
 
 # CRIANDO E NOMEANDO A JANELA -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class logincadastro(QWidget):
@@ -11,8 +11,8 @@ class logincadastro(QWidget):
 
         self.setWindowTitle("Caixa")
 
-        self.setGeometry(50,50,1200,800)
-        self.setFixedSize(1200,800)
+        self.setGeometry(560,240,800,600)
+        self.setFixedSize(800,600)
         self.setStyleSheet("""
                             background-color: #ffffff;
                            """)
@@ -44,13 +44,16 @@ class logincadastro(QWidget):
 
         self.emaillogin_edit = QLineEdit()
         self.emaillogin_edit.setStyleSheet("""
-                                            font-family: 'Arial;
-                                            font-size: 12px;
-                                            font-weight: bold;
-                                            border-radius: 15px;
-                                            background-color: #f6f6f6;
+                                            QLineEdit {
+                                                font-family: 'Arial;
+                                                font-size: 12px;
+                                                font-weight: bold;
+                                                border-radius: 15px;
+                                                background-color: #f6f6f6;
+                                            }
                                            """)
         self.emaillogin_edit.setFixedSize(300,30)
+        self.emaillogin_edit.setAlignment(Qt.AlignCenter)
 
 # -------------------------
 
@@ -63,16 +66,22 @@ class logincadastro(QWidget):
 
         self.senhalogin_edit = QLineEdit()
         self.senhalogin_edit.setStyleSheet("""
-                                            font-family: 'Arial';
-                                            font-size: 12px;
-                                            font-weight: bold;
-                                            border-radius: 15px;
+                                            QLineEdit {
+                                                font-family: 'Arial;
+                                                font-size: 12px;
+                                                font-weight: bold;
+                                                border-radius: 15px;
+                                                background-color: #f6f6f6;
+                                            }
                                            """)
+        self.senhalogin_edit.setFixedSize(300,30)
+        self.senhalogin_edit.setAlignment(Qt.AlignCenter)
 
 # REMEMBER-ME, FORGOT YOUR PASSWORD? ------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         self.rememberme_checkbox = QCheckBox("Remember-me")
         self.rememberme_checkbox.setStyleSheet("""
+                                                margin-left: 260px;
                                                 font-family: 'Arial';
                                                 font-size: 12px;
                                                """)
@@ -84,13 +93,14 @@ class logincadastro(QWidget):
                                             color: #00a2ff;
                                         """)
 
-# BOTÃO LOGIN ----------------------------
+# BOTÃO LOGIN -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         self.login_botao = QPushButton("Login")
         self.login_botao.setStyleSheet("""
                                         QPushButton {
                                             font-family: 'Times New Roman';
                                             font-size: 16px;
+                                            border-radius: 20px;
                                             color: #ffffff;
                                             background-color: #003cff;
                                         }
@@ -101,24 +111,29 @@ class logincadastro(QWidget):
                                             background-color: #1200ff;
                                         }
                                        """)
+        self.login_botao.setFixedSize(120,40)
         
 # NEU USER? CREATE AN ACCOUNT, OR ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-        self.newuser_label = QLabel ("New User?")
+        self.newuser_label = QLabel ("New User? Create an Account")
+        self.newuser_label.setText("""
+                                    New User? <span style='color:#00a2ff; text-decoration: underline;'>Create an Account</span>
+                                   """)
         self.newuser_label.setStyleSheet("""
                                             font-family: 'Times New Roman';
                                             font-size: 15px;
                                          """)
+        self.newuser_label.setAlignment(Qt.AlignCenter)
         
 # -------------------------
 
-        self.create_label = QLabel ("Create an Account")
-        self.create_label.setStyleSheet("""
-                                            font-family: 'Times New Roman';
-                                            font-size: 15px;
-                                            text-decoration: underline;
-                                            color: #00a2ff;
-                                        """)
+        #self.create_label = QLabel ("Create an Account")
+        #self.create_label.setStyleSheet("""
+                                            #font-family: 'Times New Roman';
+                                            #font-size: 15px;
+                                            #text-decoration: underline;
+                                            #color: #00a2ff;
+                                        #""")
         
 # -------------------------
 
@@ -170,18 +185,26 @@ class logincadastro(QWidget):
         self.normal = QVBoxLayout()
         self.setLayout(self.normal)
 # -------------------------
+        self.horizontal1 = QHBoxLayout()
+# -------------------------
         self.normal.addWidget(self.emailvindo_label)
         self.normal.addWidget(self.loginpfv_label)
         self.normal.addWidget(self.emaillogin_label)
         self.normal.addWidget(self.emaillogin_edit, alignment=Qt.AlignCenter)
         self.normal.addWidget(self.senhalogin_label)
-        self.normal.addWidget(self.senhalogin_edit)
-        self.normal.addWidget(self.rememberme_checkbox)
-        self.normal.addWidget(self.forgot_label)
-        self.normal.addWidget(self.login_botao)
+        self.normal.addWidget(self.senhalogin_edit, alignment=Qt.AlignCenter)
+
+        self.normal.addLayout(self.horizontal1) # Precisa ficar nessa posição
+        self.horizontal1.addWidget(self.rememberme_checkbox)
+        self.horizontal1.addWidget(self.forgot_label)
+
+        self.normal.addSpacing(5)
+        self.normal.addWidget(self.login_botao, alignment=Qt.AlignCenter)
+        self.normal.addSpacing(20)
         self.normal.addWidget(self.newuser_label)
-        self.normal.addWidget(self.create_label)
+        self.normal.addSpacing(20)
         self.normal.addWidget(self.or_label)
+        self.normal.addSpacing(20)
         self.normal.addWidget(self.facebook_button)
         self.normal.addWidget(self.google_button)
 
@@ -189,20 +212,24 @@ class logincadastro(QWidget):
 # Geometry: (50, 50, 1200, 800)
 # Lembre-se: Esquerda, Cima, Direita, Baixo (afastar das bordas)
 
+        self.normal.addStretch(1)
+# -------------------------
         self.emailvindo_label.setContentsMargins(50,100,50,0)
         self.loginpfv_label.setContentsMargins(50,0,50,40)
-        self.emaillogin_label.setContentsMargins(0,0,0,0)
-        #self.emaillogin_edit.setContentsMargins(0,0,0,0)
-        #self.senhalogin_label.setContentsMargins(0,0,0,0)
-        #self.senhalogin_edit.setContentsMargins(0,0,0,0)
-        #self.rememberme_checkbox.setContentsMargins(0,0,0,0)
-        #self.forgot_label.setContentsMargins(0,0,0,0)
-        #self.login_botao.setContentsMargins(0,0,0,0)
-        #self.newuser_label.setContentsMargins(0,0,0,0)
-        #self.create_label.setContentsMargins(0,0,0,0)
+
+        self.horizontal1.addSpacing(-100)
+        self.horizontal1.setContentsMargins(0,0,100,0)
+
         #self.or_label.setContentsMargins(0,0,0,0)
         #self.facebook_button.setContentsMargins(0,0,0,0)
         #self.google_button.setContentsMargins(0,0,0,0)
+
+# Estes são controlados pelo addStretch
+
+        #self.emaillogin_label.setContentsMargins(0,0,0,0)
+        #self.emaillogin_edit.setContentsMargins(0,0,0,0)
+        #self.senhalogin_label.setContentsMargins(0,0,0,0)
+        #self.senhalogin_edit.setContentsMargins(0,0,0,0)
 
 # ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
