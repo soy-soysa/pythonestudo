d7dcb06bf16f992ef6f5607f4077a817d695d159
soy-soysa <rodrigodesousacosta8839@gmail.com>
2025-10-25 13:59:55 -0300

    Concluindo a janela de logincadastro

--- Janelas-Python/logincadastro.py
+++ Janelas-Python/logincadastro.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap, QIcon, QFont
+from PyQt5.QtGui import QPixmap, QIcon, QFont, QColor
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QInputDialog, QMessageBox, QVBoxLayout, QHBoxLayout, QCheckBox
 
 # CRIANDO E NOMEANDO A JANELA -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -144,48 +144,33 @@ class logincadastro(QWidget):
                                     """)
         self.or_label.setAlignment(Qt.AlignCenter)
 
-# LOGIN WITH FACEBOOK, LOGIN WITH GOOGLE --------------------------------------------------------------------------------------------------------------------------------------------------------------
+# WIDGET COLORIDA, LOGIN WITH FACEBOOK, LOGIN WITH GOOGLE ---------------------------------------------------------------------------------------------------------------------------------------------
 
-        self.facebook_button = QPushButton("Login with Facebook")
-        self.facebook_button.setStyleSheet("""
-                                            QPushButton {
-                                                font-family: 'Times New Roman';
-                                                font-size: 16px;
-                                                color: #ffffff;
-                                                background-color: #3b5998;
-                                            }
-                                            QPushButton:hover {
-                                                background-color: #556b9a;
-                                            }
-                                            QPushButton:pressed {
-                                                background-color: #3b5998;
-                                            }
-                                           """)
+        self.fundopreto_widget = QWidget()
+        self.fundopreto_widget.setStyleSheet("""
+                                                background-color: #1a1a45;
+                                             """)
+
+# -------------------------
+
+        self.facebook_imagem = QLabel("Login with Facebook")
+        self.facebook_imagem.setPixmap(QPixmap("facebookicon.png").scaled(80, 80, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        self.facebook_imagem.move(100,80)
         
 # -------------------------
 
-        self.google_button = QPushButton("Login with Google")
-        self.google_button.setStyleSheet("""
-                                            QPushButton {
-                                                font-family: 'Times New Roman';
-                                                font-size: 16px;
-                                                color: #ffffff;
-                                                background-color: #db4a39;
-                                            }
-                                            QPushButton:hover {
-                                                background-color: #da7367;
-                                            }
-                                            QPushButton:pressed {
-                                                background-color: #db4a39;
-                                            }
-                                         """)
+        self.google_imagem = QLabel("Login with Facebook")
+        self.google_imagem.setPixmap(QPixmap("googleicon.png").scaled(80, 80, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        self.google_imagem.move(100,80)
 
 # ADICIONANDO AS JANELAS E LAYOUTS --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         self.normal = QVBoxLayout()
         self.setLayout(self.normal)
 # -------------------------
-        self.horizontal1 = QHBoxLayout()
+        self.horizontal = QHBoxLayout()
+# -------------------------
+        self.horizontal_layout = QHBoxLayout()
 # -------------------------
         self.normal.addWidget(self.emailvindo_label)
         self.normal.addWidget(self.loginpfv_label)
@@ -194,9 +179,9 @@ class logincadastro(QWidget):
         self.normal.addWidget(self.senhalogin_label)
         self.normal.addWidget(self.senhalogin_edit, alignment=Qt.AlignCenter)
 
-        self.normal.addLayout(self.horizontal1) # Precisa ficar nessa posição
-        self.horizontal1.addWidget(self.rememberme_checkbox)
-        self.horizontal1.addWidget(self.forgot_label)
+        self.normal.addLayout(self.horizontal) # Precisa ficar nessa posição
+        self.horizontal.addWidget(self.rememberme_checkbox)
+        self.horizontal.addWidget(self.forgot_label)
 
         self.normal.addSpacing(5)
         self.normal.addWidget(self.login_botao, alignment=Qt.AlignCenter)
@@ -205,10 +190,16 @@ class logincadastro(QWidget):
         self.normal.addSpacing(20)
         self.normal.addWidget(self.or_label)
         self.normal.addSpacing(20)
-        self.normal.addWidget(self.facebook_button)
-        self.normal.addWidget(self.google_button)
 
-# MANIPULANDO AS MARGINS ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        self.horizontal_layout.addStretch(1) # Precisa ficar nessa posição
+        self.horizontal_layout.addWidget(self.facebook_imagem)
+        self.horizontal_layout.addWidget(self.google_imagem)
+        self.horizontal_layout.addStretch(1)
+
+        self.fundopreto_widget.setLayout(self.horizontal_layout)
+        self.normal.addWidget(self.fundopreto_widget)
+
+# MANIPULANDO AS MARGINS/ESPAÇOS ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Geometry: (50, 50, 1200, 800)
 # Lembre-se: Esquerda, Cima, Direita, Baixo (afastar das bordas)
 
@@ -217,12 +208,10 @@ class logincadastro(QWidget):
         self.emailvindo_label.setContentsMargins(50,100,50,0)
         self.loginpfv_label.setContentsMargins(50,0,50,40)
 
-        self.horizontal1.addSpacing(-100)
-        self.horizontal1.setContentsMargins(0,0,100,0)
-
-        #self.or_label.setContentsMargins(0,0,0,0)
-        #self.facebook_button.setContentsMargins(0,0,0,0)
-        #self.google_button.setContentsMargins(0,0,0,0)
+        self.horizontal.addSpacing(-100)
+        self.horizontal.setContentsMargins(0,0,100,0)
+# -------------------------
+        self.horizontal_layout.setSpacing(60) #espaço entre as imagens
 
 # Estes são controlados pelo addStretch
 
@@ -230,8 +219,10 @@ class logincadastro(QWidget):
         #self.emaillogin_edit.setContentsMargins(0,0,0,0)
         #self.senhalogin_label.setContentsMargins(0,0,0,0)
         #self.senhalogin_edit.setContentsMargins(0,0,0,0)
+        #self.or_label.setContentsMargins(0,0,0,0)
+        #self.fundopreto_widget.setContentsMargins(0,0,0,400)
 
-# ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 app = QApplication(sys.argv)
 janela = logincadastro()
